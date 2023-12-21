605e6d2442f74d97d2eceab8542e5d813fbb2f22
Savely Zarkaliani <mauro.montenno@gmail.com>
2023-12-21 18:44:05 +0300

    add double, char and bool

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,1 +1,8 @@
-﻿Console.WriteLine("Hello World");
+﻿// Console.WriteLine("Hello World");
+
+double c = 55.5;
+char d = 'd';
+bool e = false;
+Console.WriteLine(c);
+Console.WriteLine(d);
+Console.WriteLine(e);
